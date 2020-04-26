4dcc3737651b9309c60e8f7bb442299d7c554e0d
Andre <00103017@uca.edu.sv>
2020-04-26 16:58:46 -0600

    ejercicio 2

--- tarea_2_00103017.asm
+++ tarea_2_00103017.asm
@@ -2,7 +2,37 @@
 
 
 section     .text
+;---------------TAREA 2----------------------------------------------------
+	mov 	DX, 0000h ;limpiando dx
+	mov 	CX, 0000h; limpiando cx
+	mov	AX, 2d;	AX Tendrá el valor inicial de 2
+	mov	CX, 2d; BX tendrá el valor de 2
+	mul 	CX
+	mov	[210h], ax
+	mov 	BX, 0000h
+
+frstWhile:	inc bx
+	mul cx
+	mov [BX+ 210h], ax
+	CMP ax, 0xFF; si es menor que 255 hexa
+	jb frstWhile
+
+scndWhile:	inc bx
+	mul cx
+	inc bx
+	mov [bx+ 210h], ax
+	cmp bx, 12d
+	jb scndWhile
+
+;---------------TAREA 3----------------------------------------------------
+;FIBONACCI
+
+
+
+
 ;---------------TAREA 1----------------------------------------------------
+	mov 	AX, 0000h ;limpiando AX
+	mov	BX, 0000h ;limpiando BX
         mov     AX, 0d
         mov     BX, 3d
         ADD     AX, BX ;AX = 3
@@ -20,9 +50,9 @@ section     .text
 	mov 	BX, 5h ;guardando el el divior
 	div	BX	; cociente queda en ax = 2 y residuo queda en DX = 1
 
-	mov 	[210h], DX ;guardando temporalmente el residuo
-	mov	[211h], AX ;guardando temporalmente el cociente
-;---------------------------------------------------------------------------------------
+	mov 	[230h], DX ;guardando temporalmente el residuo
+	mov	[231h], AX ;guardando temporalmente el cociente
+
         mov     di, 0d
         mov     cx, [len]
 
@@ -30,7 +60,7 @@ lupi:   mov     bl, [comnt+di]
         mov     [di+200h], bl
         inc     di
         loop    lupi
-
+;---------------------------------------------------------------------------------
     int 20h
 
 section     .data
