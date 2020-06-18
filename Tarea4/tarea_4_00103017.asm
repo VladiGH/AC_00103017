e78a0f05c2208ee70d0338c3e1502f613f153ccf
Andre <00103017@uca.edu.sv>
2020-06-17 22:28:30 -0600

    fixing

--- Tarea4/tarea_4_00103017.asm
+++ Tarea4/tarea_4_00103017.asm
@@ -9,7 +9,9 @@ section .text
 lupi:	call 	kb
 	cmp 	al, "$"
 	je	mostrar
+	;sub	al, 30h
 	mov	[300h+si], al ; CS:0300h en adelante
+	
 	inc 	si
 	jmp 	lupi
 
@@ -18,47 +20,43 @@ mostrar:mov	byte [300h+si], "$"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	call 	kb	; solo detenemos la ejecución
 
-;;;;;;segundo mensaje
-	mov	dx, prome
-	call	print
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	call 	prom 	;calculo promedio
 	mov	byte [331h], "$"
 	call 	w_strng3
 
-	mov	al, "1"
+	mov	al, 1
 	cmp	[330h], al
 	je	nota1
 
-	mov	al, "2"
+	mov	al, 2
 	cmp	[330h], al
 	je	nota2
 
-	mov	al, "3"
+	mov	al, 3
 	cmp	[330h], al
 	je	nota3
 
-	mov	al, "4"
+	mov	al, 4
 	cmp	[330h], al
 	je	nota4
 
-	mov	al, "5"
+	mov	al, 5
 	cmp	[330h], al
 	je	nota5
 
-	mov	al, "6"
+	mov	al, 6
 	cmp	[330h], al
 	je	nota6
 
-	mov	al, "7"
+	mov	al, 7
 	cmp	[330h], al
 	je	nota7
 
-	mov	al, "8"
+	mov	al, 8
 	cmp	[330h], al
 	je	nota8
 		
-	mov	al, "9"
+	mov	al, 9
 	cmp	[330h], al
 	je	nota9
 
@@ -101,21 +99,23 @@ print:	mov	ah, 09h
 prom:	mov 	ax, 0000h
 	mov 	bx, 0000h
 	mov	al, [300h]
+	sub 	al, 30h
 	mov	bl, [301h]
+	sub 	bl,30h
 	add	al, bl
 	mov	bl, [302h]
+	sub 	bl,30h
 	add	al, bl
 	mov	bl, [303h]
+	sub 	bl,30h
 	add	al, bl
 	mov	bl, [304h]
+	sub 	bl,30h
 	add	al, bl
 	mov	[310h], al
 	mov 	DX, 0000h; limpiando dx
 	mov 	BX, 5h ;guardando el el divior
 	div	BX	; cociente queda en ax = 2 y residuo queda en DX = 1
-	;mov 	[320h], DX ;guardando temporalmente el residuo
-	AAA
-	or	al, 30h
 	mov	[330h], al ;guardando temporalmente el cociente
 	ret
 
@@ -160,7 +160,6 @@ nota10:	mov 	dx, msg10
 	ret
 
 section .data
-prome	db	" Prom-> $"
 
 bien	db	"Ingresa 5 caracteres num-> $"
 
